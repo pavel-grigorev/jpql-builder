aab260b668cccee379c74365e9008488823cb9b2
Pavel Grigorev <grigoriev-p@haulmont.com>
2020-09-08 21:43:25 +0400

    Refactor DefaultInstanceFactory

--- src/main/java/org/test/factory/DefaultInstanceFactory.java
+++ src/main/java/org/test/factory/DefaultInstanceFactory.java
@@ -2,14 +2,14 @@ package org.test.factory;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.sql.Date;
+import java.util.Calendar;
 import java.util.Currency;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 import java.util.UUID;
 
 public class DefaultInstanceFactory implements InstanceFactory {
@@ -37,13 +37,14 @@ public class DefaultInstanceFactory implements InstanceFactory {
     instanceCreators.put(BigDecimal.class, () -> new BigDecimal(0));
     instanceCreators.put(BigInteger.class, () -> BigInteger.valueOf(128));
     instanceCreators.put(UUID.class, UUID::randomUUID);
-    instanceCreators.put(Date.class, () -> new Date(0));
-    instanceCreators.put(GregorianCalendar.class, GregorianCalendar::new);
+    instanceCreators.put(java.util.Date.class, java.util.Date::new);
+    instanceCreators.put(java.sql.Date.class, () -> new java.sql.Date(0));
+    instanceCreators.put(Calendar.class, GregorianCalendar::new);
     instanceCreators.put(Locale.class, () -> new Locale("en"));
-    instanceCreators.put(SimpleTimeZone.class, () -> new SimpleTimeZone(0, ""));
+    instanceCreators.put(TimeZone.class, () -> new SimpleTimeZone(0, ""));
     instanceCreators.put(Currency.class, CurrencyFactory::newInstance);
     instanceCreators.put(byte[].class, () -> new byte[0]);
-    instanceCreators.put(HashMap.class, HashMap::new);
+    instanceCreators.put(Map.class, HashMap::new);
   }
 
   public <T> void add(Class<T> type, InstanceCreator<T> instanceCreator) {
@@ -57,20 +58,8 @@ public class DefaultInstanceFactory implements InstanceFactory {
       return (T) enumFactory.newInstance(type);
     }
 
-    Optional<InstanceCreator<?>> instanceCreator = getInstanceCreator(type);
-    if (instanceCreator.isPresent()) {
-      return (T) instanceCreator.get().newInstance();
-    }
-
-    return type.newInstance();
-  }
-
-  private Optional<InstanceCreator<?>> getInstanceCreator(Class<?> type) {
-    return instanceCreators
-        .keySet()
-        .stream()
-        .filter(type::isAssignableFrom)
-        .findFirst()
-        .map(instanceCreators::get);
+    return (T) instanceCreators
+        .getOrDefault(type, type::newInstance)
+        .newInstance();
   }
 }
--- src/test/java/org/test/factory/DefaultCollectionInstanceFactoryTest.java
+++ src/test/java/org/test/factory/DefaultCollectionInstanceFactoryTest.java
@@ -1,6 +1,5 @@
 package org.test.factory;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -12,28 +11,26 @@ import static org.junit.Assert.assertNotSame;
 public class DefaultCollectionInstanceFactoryTest {
   @Test
   public void newList() throws ReflectiveOperationException {
-    assertNotSame(factory.newInstance(List.class), factory.newInstance(List.class));
+    factoryCreatesUniqueInstance(List.class);
   }
 
   @Test
   public void newSet() throws ReflectiveOperationException {
-    assertNotSame(factory.newInstance(Set.class), factory.newInstance(Set.class));
+    factoryCreatesUniqueInstance(Set.class);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void unsupportedCollection() throws ReflectiveOperationException {
-    assertNotSame(factory.newInstance(Queue.class), factory.newInstance(Queue.class));
+    factoryCreatesUniqueInstance(Queue.class);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void notCollection() throws ReflectiveOperationException {
-    assertNotSame(factory.newInstance(String.class), factory.newInstance(String.class));
+    factoryCreatesUniqueInstance(String.class);
   }
 
-  private CollectionInstanceFactory factory;
-
-  @Before
-  public void setup() {
-    factory = new DefaultCollectionInstanceFactory();
+  private void factoryCreatesUniqueInstance(Class<?> type) throws ReflectiveOperationException {
+    CollectionInstanceFactory factory = new DefaultCollectionInstanceFactory();
+    assertNotSame(factory.newInstance(type), factory.newInstance(type));
   }
 }
