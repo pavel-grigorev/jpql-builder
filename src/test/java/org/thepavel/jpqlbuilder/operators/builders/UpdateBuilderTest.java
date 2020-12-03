f762cd09c42551cc2680af535e2141ae8473e610
Pavel Grigorev <pavel@thepavel.org>
2020-12-03 21:16:07 +0300

    Tests for UpdateBuilder and UpdateChain

---
+++ src/test/java/org/thepavel/jpqlbuilder/operators/builders/UpdateBuilderTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020 Pavel Grigorev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.thepavel.jpqlbuilder.operators.builders;
+
+import org.junit.Test;
+import org.thepavel.jpqlbuilder.functions.JpqlFunction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.thepavel.jpqlbuilder.DummyFunction.dummy;
+import static org.thepavel.jpqlbuilder.operators.builders.UpdateBuilder.set;
+
+public class UpdateBuilderTest {
+  @Test
+  public void toValue() {
+    UpdateChain chain = new UpdateChain();
+    UpdateBuilder<String, UpdateChain> builder = new UpdateBuilder<>(chain, "A");
+    Map<Object, Object> updates = builder.to("B").getUpdates();
+
+    assertEquals(
+        new HashMap<Object, Object>() {{
+          put("A", "B");
+        }},
+        updates
+    );
+  }
+
+  @Test
+  public void toFunction() {
+    UpdateChain chain = new UpdateChain();
+    UpdateBuilder<String, UpdateChain> builder = new UpdateBuilder<>(chain, "A");
+
+    JpqlFunction<String> function = dummy("B");
+    Map<Object, Object> updates = builder.to(function).getUpdates();
+
+    assertEquals(
+        new HashMap<Object, Object>() {{
+          put("A", function);
+        }},
+        updates
+    );
+  }
+
+  @Test
+  public void staticConstructor() {
+    assertEquals(
+        new HashMap<Object, Object>() {{
+          put("A", "B");
+        }},
+        set("A").to("B").getUpdates()
+    );
+  }
+}
---
+++ src/test/java/org/thepavel/jpqlbuilder/operators/builders/UpdateChainTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 Pavel Grigorev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.thepavel.jpqlbuilder.operators.builders;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class UpdateChainTest {
+  @Test
+  public void set() {
+    UpdateChain chain = new UpdateChain();
+    chain.set("A").to("B");
+
+    assertEquals(
+        new HashMap<Object, Object>() {{
+          put("A", "B");
+        }},
+        chain.getUpdates()
+    );
+  }
+
+  @Test
+  public void addUpdate() {
+    UpdateChain chain = new UpdateChain();
+    chain.addUpdate("X", "Y");
+
+    assertEquals(
+        new HashMap<Object, Object>() {{
+          put("X", "Y");
+        }},
+        chain.getUpdates()
+    );
+  }
+}
